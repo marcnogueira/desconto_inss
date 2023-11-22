09abcc036846fac76b5738f91099e575cf0bbc8a
Marcelo Nogueira <marcelo.nogueira@me.com>
2023-11-22 09:50:49 -0300

    refactoring discount calculation service

--- app/services/discount_calculation_service.rb
+++ app/services/discount_calculation_service.rb
@@ -3,7 +3,7 @@ class DiscountCalculationService
 
   # TODO: criar as classes de faixa salarial automaticamente a partir de um arquivo de configuração
   def initialize(salary)
-    @salary = salary
+    @salary = salary.to_f
     @first_band = FirstSalaryBand.new
     @second_band = SecondSalaryBand.new
     @third_band = ThirdSalaryBand.new
@@ -12,7 +12,7 @@ class DiscountCalculationService
     @first_band.next_band = @second_band
     @second_band.next_band = @third_band
     @third_band.next_band = @fourth_band
-    @discount, @aliquot, @salary_band = @first_band.calculate(salary, 0)
+    @discount, @aliquot, @salary_band = @first_band.calculate(@salary, 0)
   end
 
 end
